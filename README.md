ab143a3bc759b6b23770040364936d0729aac7b1
thmichaellin <thmichaellin@gmail.com>
2024-03-08 22:29:47 +0100

    Upload from local

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# master_applicant_assignment
-Master Data Science Applicant Assignment
+# Project Title:<br> Master Data Science Applicant Assignment
+## Name: Michael Lin
+
+# Description:
+Assignment for application Data Science track in Information Studies Master’s programme
+It is often that data available in databases, or flat files of a company/organization need to be pre-
+processed, and stored in the appropriate data structures so that they can be easily used by data
+mining or machine learning algorithms. This data may come in a variety of forms and modalities (e.g.
+structured records, unstructured, or semi-structured text, images, etc.). In this assignment we will
+consider a sample dataset (collection.txt) that contains three articles from LA Times in a semi-
+structured format. The tags in the collection dictate the beginning and the end of an article (<doc>
+and </doc>), the article id, the headline of the article and the main text (<text> and </text>), along
+with several other information on the article.
+PART I: Design and code up a class that can pre-process and store the LA Times articles. Specifically,
+the methods of the class should take as an input the LA Times articles collection, extract each article
+in the collection, and construct a hash table, the key of which is a word (in the collection) and the
+value a linked list of all the document that contain this word, and the count of the word in each
+document. For example, if the word “the” appears in all three articles, 20 times in the first, 34 times
+in the second, and 12 times in the third, while the word “author” appears 7 times in the first, 3 times
+in the second and does not appear at all in the third, the hash table should look as follows:
+[the] -> [1, 20] -> [2, 34] -> [3, 12]
+[author] -> [1, 7] -> [2,3]
+Create an object of the type of your class and use the data collection to initiate it. Think about how
+could you handle different forms of the same word, e.g. "author", "Author", "authors". Turn your
+code in a pdf document.
+PART II: Generate a plot (histogram should be good enough) of the count distribution of the words in
+all documents (that is, the x-axis is the number of times a word appears in the entire collection -
+total count -, and the y-axis the frequency of that count). Characterize this distribution.
+Have a look at the example solution to compare.
+
+# Prerequisites:
+### Install required modules
+`pip install -r requirements.txt`
---
+++ assignment.py
@@ -0,0 +1,172 @@
+import string
+import re
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
+
+
+def find_all(text, word):
+    """
+    Find all occurrences of a word in a text.
+
+    :param text: (str) The text to search
+    :param word: (str) The word to find
+    :return: (list(int)) List of word indices
+    """
+
+    indices = []
+    start_index = 0
+    while True:
+        index = text.find(word, start_index)
+
+        # Break when no more occurrences can be found
+        if index == -1:
+            break
+
+        # Append index of string and update new starting index for find
+        indices.append(index)
+        start_index = index + len(word)
+
+    return indices
+
+
+class Node:
+    def __init__(self, data=None):
+        """
+        Initialize a Node object for a linked list.
+
+        :param data: (any) The data to store in the node
+        """
+
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        """
+        Initialize a LinkedList object.
+        """
+
+        self.head = Node()
+
+    def append_node(self, data):
+        """
+        Append a new node to the linked list.
+
+        :param data: The data to store in the new node
+        """
+
+        new_node = Node(data)
+        current_node = self.head
+
+        # Find end of linked list
+        while current_node.next is not None:
+            current_node = current_node.next
+        current_node.next = new_node
+
+    def get_list(self):
+        """
+        Get the elements of the linked list as a list.
+
+        :return: (list(any)) A list of elements of the linked list
+        """
+        list_elements = []
+        current_node = self.head
+
+        # Find end of linked list
+        while current_node.next is not None:
+            current_node = current_node.next
+            list_elements.append(current_node.data)
+
+        return list_elements
+
+
+class Article:
+    def __init__(self, filename):
+        """
+        Initialize an Article object.
+
+        :param filename: (str) The filename of the text
+        """
+        self.filename = filename
+        self.file = None
+        self.text_start = None
+        self.text_end = None
+        self.words = {}
+        self.read_file()
+        self.find_start_end()
+        self.lemmatizer = WordNetLemmatizer()
+        self.populate()
+
+    def read_file(self):
+        """
+        Read the contents of the file.
+        """
+        with open(self.filename, 'r') as f:
+            self.file = f.read()
+
+    def find_start_end(self):
+        """
+        Find the start and end indices of articles in text.
+        """
+
+        self.text_start = find_all(self.file, "<text>")
+        self.text_end = find_all(self.file, "</text>")
+
+    def populate(self):
+        """
+        Populate dictionary of words with linked list of word counts.
+        """
+
+        for i in range(len(self.text_end)):
+            text = self.file[self.text_start[i] + 5:self.text_end[i] - 1]
+
+            # Remove html tags
+            text = re.sub('<[^<]+?>', '', text)
+
+            # Remove punctuation and set convert to lowercase
+            text = text.translate(str.maketrans('', '', string.punctuation))
+            text = text.lower()
+
+            # Lemmatize (root of word) words in article and count unique words
+            words = text.split()
+            words = [self.lemmatizer.lemmatize(word, pos='v')
+                     for word in words]
+            unique_words, counts = np.unique(words, return_counts=True)
+
+            # Add article number to counts
+            counts = [[i + 1, count] for count in counts]
+
+            # Zip words to counts
+            word_count = dict(zip(unique_words, counts))
+
+            # Add word count as node, initialize linked list if word not found
+            for word in unique_words:
+                if hash(word) not in self.words.keys():
+                    self.words[hash(word)] = LinkedList()
+                self.words[hash(word)].append_node(word_count[word])
+
+    def plot(self):
+        """
+        Plot the distribution of the word counts.
+        """
+        frequencies = []
+        for key in self.words:
+            for element in self.words[key].get_list():
+                frequencies.append(element[1])
+
+        plt.hist(frequencies, bins=len(self.words))
+        plt.xlabel("Frequency of Word in Collection")
+        plt.ylabel("Frequency of Count")
+        plt.title("Count Distribution of Words in Collection")
+
+
+def main():
+    collection = Article('collection.txt')
+    collection.plot()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
